83199a376973bebd8cc5114050f0732449c596c4
VirginiaCreativa <virginiavelasquezsoto@gmail.com>
2022-11-17 15:55:37 -0500

    Upload Folder

---
+++ src/styles/variable.js
@@ -0,0 +1,15 @@
+const color = {
+  red: '#e23836',
+  yellow: '#fcdd00',
+  blue: '#002149',
+  white: '#fff',
+  blue_ligth: '#0068ff',
+  blue_dark: '#004cba',
+  grey_1: '#F7F7F7',
+  grey_2: '#EEEEEE',
+  grey_3: '#929AAB',
+  grey_4: '#393E46',
+  text: '#393E46',
+};
+
+export default color;
